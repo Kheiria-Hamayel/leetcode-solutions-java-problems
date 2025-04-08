9dd62fbad628ce5c4b7267248ca8b39f3d943ce7
Kheiria-Hamayel <68916595+Kheiria-Hamayel@users.noreply.github.com>
2025-04-09 00:18:06 +0300

    Time: 229 ms (5.15%), Space: 48.7 MB (5.37%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -40,36 +40,46 @@ class Solution {
             List<Integer> sIndex1 = sMap.get(s1);
             List<Integer> tIndex1 = tMap.get(t1);
 
+            Collections.sort(sIndex1);
+            Collections.sort(tIndex1);
+
             if(sIndex1 != null && tIndex1 != null && sIndex1.size() != tIndex1.size()) return false;
 
             for(int j = 0 ; j < sIndex1.size() ; j++)
             {
-                if(sIndex1.get(j) != tIndex1.get(j)) return false;
+                if(!sIndex1.get(j).equals(tIndex1.get(j))) return false;
             }
 
         }
 
         return true;
 
-        // for(int i = 0 ; i < t.length(); i++)
-        // {
-        //     if(sMap.get(i) != null && tMap.get(i) != null && sMap.get(i).size() != tMap.get(i).size()) return false;
-        //     else 
-        //     {
-        //         Character t1 = t.charAt(i);
-        //         Character s1 = s.charAt(i);
-        //         if(tMap.get(i) != null)
-        //         {
-        //         for(int j = 0 ; j < tMap.get(t1).size(); j++)
-        //         {
-        //             if(tMap.get(t1).get(j) != sMap.get(s1).get(j)) return false;
-        //         }
-        //         }
-        //     }
-        // }
-
-        // return true;
-
-
     }
 }
+
+// Much simpler soluation 
+
+/*
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Integer> charIndexS = new HashMap<>();
+        HashMap<Character, Integer> charIndexT = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!charIndexS.containsKey(s.charAt(i))) {
+                charIndexS.put(s.charAt(i), i);
+            }
+
+            if (!charIndexT.containsKey(t.charAt(i))) {
+                charIndexT.put(t.charAt(i), i);
+            }
+
+            if (!charIndexS.get(s.charAt(i)).equals(charIndexT.get(t.charAt(i)))) {
+                return false;
+            }
+        }
+
+        return true;        
+    }
+}
+*/
