d357ef818c46dc857bcde555bf2677a9f8746317
Kheiria-Hamayel <68916595+Kheiria-Hamayel@users.noreply.github.com>
2025-04-09 01:06:01 +0300

    Time: 28 ms (5.45%), Space: 44.8 MB (27.06%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()  ) return false;
+
+        Map<Character, Integer> sMap = new HashMap<>();
+        Map<Character, Integer> tMap = new HashMap<>();
+
+
+        for(int i= 0; i < s.length() ; i++)
+        {
+            sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i),1) + 1);
+            tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i),1) + 1);
+        }
+
+        for(Character c : sMap.keySet() )
+        {
+            Integer valS = sMap.get(c);
+            Integer valt = tMap.get(c);
+            if(!valS.equals(valt)) return false;
+        }
+return true;
+    }
+}
