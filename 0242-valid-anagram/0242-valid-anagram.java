ea062e2f39de8afd79d5fa04d489925b2e2107c2
Kheiria-Hamayel <68916595+Kheiria-Hamayel@users.noreply.github.com>
2025-04-11 11:34:49 +0300

    Time: 2 ms (97.69%), Space: 43.3 MB (74.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,23 +1,49 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        int [] arr = new int[26];
         if(s.length() != t.length()  ) return false;
-
-        Map<Character, Integer> sMap = new HashMap<>();
-        Map<Character, Integer> tMap = new HashMap<>();
-
-
-        for(int i= 0; i < s.length() ; i++)
+        for(char ch : s.toCharArray())
         {
-            sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i),1) + 1);
-            tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i),1) + 1);
+            arr[ch - 'a']++;
+        }
+        for(char ch : t.toCharArray())
+        {
+            int tChar = arr[ch - 'a'];
+            if(tChar == 0 ) return false;
+            arr[ch - 'a']--;
         }
 
-        for(Character c : sMap.keySet() )
+        for(int i =0 ; i < arr.length; i++)
         {
-            Integer valS = sMap.get(c);
-            Integer valt = tMap.get(c);
-            if(!valS.equals(valt)) return false;
+            if(arr[i] > 0) return false;
         }
-       return true;
+    return true;
+    
     }
 }
+
+
+
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if(s.length() != t.length()  ) return false;
+
+//         Map<Character, Integer> sMap = new HashMap<>();
+//         Map<Character, Integer> tMap = new HashMap<>();
+
+
+//         for(int i= 0; i < s.length() ; i++)
+//         {
+//             sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i),1) + 1);
+//             tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i),1) + 1);
+//         }
+
+//         for(Character c : sMap.keySet() )
+//         {
+//             Integer valS = sMap.get(c);
+//             Integer valt = tMap.get(c);
+//             if(!valS.equals(valt)) return false;
+//         }
+//        return true;
+//     }
+// }
