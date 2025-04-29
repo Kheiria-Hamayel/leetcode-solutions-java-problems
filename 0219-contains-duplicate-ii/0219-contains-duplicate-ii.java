a8c9b3448e533257afb7e554172ae32677feb3d5
Kheiria-Hamayel <68916595+Kheiria-Hamayel@users.noreply.github.com>
2025-04-29 22:36:09 +0300

    Time: 22 ms (37.91%), Space: 59.6 MB (36.28%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -15,25 +15,6 @@ class Solution {
             set.add(nums[i]);
         }
 
-return false;
-
-
-
-        // HashSet<Integer> w = new HashSet<>();
-        // int l = 0;
-
-        // for(int i =0 ; i< nums.length ; i++ )
-        // {
-        //     if(i - l > k) 
-        //     {
-        //         w.remove(nums[l]);
-        //         l++;
-        //     }
-        //     if(w.contains(nums[i])) return true;
-        //     w.add(nums[i]);
-
-        // }
-
-        // return false;
+        return false;
     }
 }
