bb100d305368ab28d0291846eb257338938f664c
Kheiria-Hamayel <68916595+Kheiria-Hamayel@users.noreply.github.com>
2025-05-03 23:24:53 +0300

    Time: 2 ms (78.95%), Space: 44.1 MB (95.2%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,34 +1,37 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set<Character> [] rows = new HashSet[9];
-        Set<Character> [] cols = new HashSet[9];
-        Set<Character> [] boxes = new HashSet[9];
+       Set<Character> [] rows = new HashSet [9];
+       Set<Character> [] cols = new HashSet [9];
+       Set<Character> [] box = new HashSet [9];
 
         for(int i = 0 ; i < 9 ; i++)
         {
             rows[i] = new HashSet<>();
             cols[i] = new HashSet<>();
-            boxes[i] = new HashSet<>();
+            box[i] = new HashSet<>();
         }
 
-        for(int i = 0 ; i < 9 ; i++ )
+        for(int i = 0 ; i < 9 ; i++)
             for(int j = 0 ; j < 9 ; j++)
-            {
-                if(board[i][j] == '.')
                 {
-                    continue;
+                    if(board[i][j] == '.')
+                    {
+                        continue;
+                    }
+
+                    Character x = board[i][j];
+                    int boxId = 3 * (i/3) + (j/3);
+
+                    if(!rows[i].add(x) || !cols[j].add(x) || !box[boxId].add(x))
+                    {
+                        return false;
+                    }
+
                 }
 
-                Character z = board[i][j]; 
-                int boxIndex = 3* (i/3) + (j/3);
 
-                if(!rows[i].add(z) || !cols[j].add(z) || !boxes[boxIndex].add(z))
-                {
-                    return false;
-                }
-            }
+return true;
 
-        return true;
     }
 }
 
