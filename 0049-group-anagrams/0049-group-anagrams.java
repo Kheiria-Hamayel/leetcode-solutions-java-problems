e6844225b8b9fffb288ce7739c6a05e269b170e7
Kheiria-Hamayel <68916595+Kheiria-Hamayel@users.noreply.github.com>
2025-05-08 22:56:51 +0300

    Time: 9 ms (35.83%), Space: 47.7 MB (82.3%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,5 +1,50 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> ans = new ArrayList<>();
+        List<String>sub = new ArrayList<>();
+        Map<String,List<Integer>> map = new HashMap<>();
+        if(strs.length == 1) 
+        {
+            sub.add(strs[0]);
+            ans.add(sub);
+            return ans;
+        }
+
+        for(int i = 0; i < strs.length ; i++)
+        {
+            char [] chars = strs[i].toCharArray();
+            Arrays.sort(chars);
+            String k = new String(chars);
+            if(!map.containsKey(k))
+            {
+                map.put(k, new ArrayList<>());
+            }
+            map.get(k).add(i);
+        }
+
+        for(Map.Entry<String, List<Integer>> entry : map.entrySet())
+        {
+            List<Integer> list = entry.getValue();
+            List<String>sub1 = new ArrayList<>();
+
+            for(Integer n : list)
+            {
+                sub1.add(strs[n]);
+            }
+            ans.add(sub1);
+        }
+
+return ans;
+
+
+        
+    }
+}
+
+
+/*
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
           if (strs == null || strs.length == 0) return new ArrayList<>();
 
         Map<String, List<String>> map = new HashMap<>();
@@ -15,3 +60,4 @@ class Solution {
         return new ArrayList<>(map.values());
     }
 }
+*/
